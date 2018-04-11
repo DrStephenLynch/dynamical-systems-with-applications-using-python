22e4e07f5c3f669526c155fc38f10fc4bf169e2a
DrStephenLynch <s.lynch@mmu.ac.uk>
2018-04-11 15:00:30 +0100

    Change filename to f_mu.py and update file

--- IDLE-files/fmu.py
+++ IDLE-files/fmu.py
@@ -1,4 +1,4 @@
-# The logistic function - save file as fmu.py.
+# The logistic function - save file as f_mu.py.
 # Run the Module (or type F5).
 """
 You can write your own text here.
@@ -7,5 +7,5 @@ Created on Tue Jan 23 09:23:47 2018
 
 @author: sladmin
 """
-def fmu(mu, x):
+def f_mu(mu, x):
     return mu*x*(1-x)
